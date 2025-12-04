4c9c63275d93dc3005730bb195e8bf3950d2d4a7
Bobby R. Ward <bobbyrward@gmail.com>
2025-12-04 00:14:41 -0600

    feat: booksite integration and userscript

---
+++ docs/userscripts/README.md
@@ -0,0 +1,3 @@
+# Stronghold Userscripts
+
+This directory contains userscripts for integrating external websites with Stronghold.
---
+++ internal/www/api/book_torrent_dl.go
@@ -0,0 +1,62 @@
+package api
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/bobbyrward/stronghold/internal/qbit"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+// SearchASINRequest contains the request body for ASIN search
+type BookTorrentDLRequest struct {
+	Category   string `json:"category" validate:"required"`
+	TorrentID  string `json:"torrent_id" validate:"required"`
+	TorrentURL string `json:"torrent_url" validate:"required"`
+}
+
+// SearchASIN searches for audiobooks by title and author, returning ASINs and metadata
+func DownloadBookTorrent(db *gorm.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := c.Request().Context()
+
+		var req BookTorrentDLRequest
+		if err := BindRequest(c, ctx, &req); err != nil {
+			return BadRequest(c, ctx, "invalid request body")
+		}
+
+		if err := ValidateRequest(c, ctx, &req); err != nil {
+			return BadRequest(c, ctx, "invalid request body")
+		}
+
+		qbitClient, err := qbit.CreateClient()
+		if err != nil {
+			return InternalError(c, ctx, "failed to create qBittorrent client", err)
+		}
+
+		slog.InfoContext(ctx, "Downloading book torrent",
+			slog.String("category", req.Category),
+			slog.Any("torrent_id", req.TorrentID),
+		)
+
+		err = qbitClient.AddTorrentFromUrlCtx(
+			ctx,
+			req.TorrentURL,
+			map[string]string{
+				"autoTMM":  "true",
+				"category": req.Category,
+			},
+		)
+		if err != nil {
+			return InternalError(c, ctx, "failed to add torrent to qbit", err)
+		}
+
+		slog.InfoContext(ctx, "Successfully downloaded book torrent",
+			slog.String("category", req.Category),
+			slog.Any("torrent_id", req.TorrentID),
+		)
+
+		return c.JSON(http.StatusOK, map[string]string{})
+	}
+}
